194e5565a51f96b162093eac16e4e77be677f554
Matthew Pocock <turingatemyhamster@gmail.com>
2015-01-30 23:14:43 +0000

    Added entities, literals and anonymous individuals.

--- core/src/main/scala/uk/co/turingatemyhamster/owl2/Entities_Literals_AnonymousIndividuals.scala
+++ core/src/main/scala/uk/co/turingatemyhamster/owl2/Entities_Literals_AnonymousIndividuals.scala
@@ -1,10 +1,59 @@
-package uk.co.turingatemyhamster.owl2
+package uk.co.turingatemyhamster
+package owl2
 
 /**
- *
+ * An abstraction of: http://www.w3.org/TR/2012/REC-owl2-syntax-20121211/#Entities.2C_Literals.2C_and_Anonymous_Individuals
  *
  * @author Matthew Pocock
  */
 trait Entities_Literals_AnonymousIndividuals {
 
+  importedPackages : owl2.Iri =>
+
+  type ClassExpression
+
+  type Entity
+  // Entity: def entityIRI: IRI
+
+  def Entity : EntityApi
+
+  trait EntityApi {
+    def unapply(entity: Entity): Some[IRI]
+  }
+
+  type DataRange
+
+  def DataRange : DataRangeApi
+
+  type UnlimitedNatural
+
+  trait DataRangeApi {
+    def unapply(dataRange: DataRange): Option[UnlimitedNatural]
+  }
+
+  type Individual
+
+
+
+  type Class <: ClassExpression with Entity
+
+  type ObjectProperty <: Entity
+
+  type DataProperty <: Entity
+
+  type AnnotationProperty <: Entity
+
+  // constraint: arity == 1
+  type Datatype <: Entity with DataRange
+
+  type NamedIndividual <: Entity with Individual
+
+  type AnonymousIndividual
+  // AnonymousIndividual: def nodeId: String
+
+  def AnonymousIndividual: AnonymousIndividualApi
+
+  trait AnonymousIndividualApi {
+    def unapply(ai: AnonymousIndividual): Some[String]
+  }
 }
--- core/src/main/scala/uk/co/turingatemyhamster/owl2/ast/Entities_Literals_AnonymousIndividualsImpl.scala
+++ core/src/main/scala/uk/co/turingatemyhamster/owl2/ast/Entities_Literals_AnonymousIndividualsImpl.scala
@@ -1,10 +1,74 @@
-package uk.co.turingatemyhamster.owl2.ast
+package uk.co.turingatemyhamster
+package owl2
+package ast
 
 /**
  *
  *
  * @author Matthew Pocock
  */
-class Entities_Literals_AnonymousIndividualsImpl {
+trait Entities_Literals_AnonymousIndividualsImpl extends owl2.Entities_Literals_AnonymousIndividuals {
 
+  importedPackages : owl2.Iri with ast.IriImpl =>
+
+  override final type ClassExpression = ast.ClassExpression
+
+  override final type Entity = ast.Entity
+
+  override final val Entity: EntityApi = new EntityApi {
+    override def unapply(entity: Entity) = Some(entity.entityIRI)
+  }
+
+  override final type DataRange = ast.DataRange[UnlimitedNatural]
+
+  override final type Individual = ast.Individual
+
+  override final type Datatype = ast.Datatype[UnlimitedNatural]
+
+  override final type ObjectProperty = ast.ObjectProperty
+
+  override final type DataProperty = ast.DataProperty
+
+  override final type AnnotationProperty = ast.AnnotationProperty
+
+  override final type NamedIndividual = ast.NamedIndividual
+
+  override final type AnonymousIndividual = ast.AnonymousIndividual
 }
+
+trait ClassExpression
+
+trait Entity {
+  def entityIRI: IRI
+}
+
+trait DataRange[UnlimitedNatural] {
+  def arity: UnlimitedNatural
+}
+
+trait Individual
+
+
+
+case class Class(entityIRI: IRI) extends ClassExpression with Entity
+
+// rdfs:Literal or a datatype in the datatype map or not a reserved vocabulary IRI
+case class Datatype[UnlimitedNatural](entityIRI: IRI,
+                                      arity: UnlimitedNatural,
+                                      datatype: Literal) extends Entity with DataRange[UnlimitedNatural]
+
+case class ObjectProperty(entityIRI: IRI) extends Entity
+
+case class DataProperty(entityIRI: IRI) extends Entity
+
+case class AnnotationProperty(entityIRI: IRI) extends Entity
+
+case class NamedIndividual(entityIRI: IRI) extends Entity with Individual
+
+case class AnonymousIndividual(nodeID: String) extends Individual
+
+sealed trait Literal
+
+case class TypedLiteral[UnlimitedNatural](value: String, datatype: Datatype[UnlimitedNatural]) extends Literal
+case class StringLiteralNoLangauge[UnlimitedNatural](value: String) extends Literal
+case class StringLiteralWithLanguage[UnlimitedNatural](value: String, language: String) extends Literal
