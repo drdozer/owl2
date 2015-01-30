4e6f7abcf23ca0e677e89941d655888d80d5911d
Matthew Pocock <turingatemyhamster@gmail.com>
2015-01-30 23:14:12 +0000

    Added api and impl for entities, literals and anonymous individuals.

---
+++ core/src/main/scala/uk/co/turingatemyhamster/owl2/Entities_Literals_AnonymousIndividuals.scala
@@ -0,0 +1,10 @@
+package uk.co.turingatemyhamster.owl2
+
+/**
+ *
+ *
+ * @author Matthew Pocock
+ */
+trait Entities_Literals_AnonymousIndividuals {
+
+}
---
+++ core/src/main/scala/uk/co/turingatemyhamster/owl2/ast/Entities_Literals_AnonymousIndividualsImpl.scala
@@ -0,0 +1,10 @@
+package uk.co.turingatemyhamster.owl2.ast
+
+/**
+ *
+ *
+ * @author Matthew Pocock
+ */
+class Entities_Literals_AnonymousIndividualsImpl {
+
+}
